db97da42af58e507faae405c50cf44228dd36686
Spencer Albrecht <s.spencer.a@gmail.com>
2016-09-24 14:26:29 -0700

    Created makefiles using macros

---
+++ lab1/HelloUser2.java
@@ -0,0 +1,14 @@
+//--------------------------------------------------------------------
+// Spencer Albrecht
+// salbrech
+// 12M
+// Simple class for testing makefiles
+// HelloUser2.java
+// -------------------------------------------------------------------
+class HelloUser2 {
+     public static void main ( String[] args) {
+          System.out.println("******************");
+          System.out.println("***** HELLO ******");
+          System.out.println("******************");
+     } 
+}
