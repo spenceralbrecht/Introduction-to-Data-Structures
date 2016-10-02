ffb86d11f9567852c5337fed81dc669703d1abc7
spenceralbrecht <s.spencer.a@gmail.com>
2016-10-01 18:01:09 -0700

    Finished method three

--- pa1/pa1.java
+++ pa1/pa1.java
@@ -14,12 +14,20 @@ class pa1 {
       // System.out.println(Arrays.toString(backward));
 
       // Testing Statements for reverseArray2
-      System.out.println("Reverse Part of the Array");
-      reverseArray2(forward, 4, backward);
-      System.out.println(Arrays.toString(backward));
-      System.out.println("Reverse the Whole Array");
-      reverseArray2(forward, forward.length, backward);
-      System.out.println(Arrays.toString(backward));
+      // System.out.println("Reverse Part of the Array");
+      // reverseArray2(forward, 4, backward);
+      // System.out.println(Arrays.toString(backward));
+      // System.out.println("Reverse the Whole Array");
+      // reverseArray2(forward, forward.length, backward);
+      // System.out.println(Arrays.toString(backward));
+
+      // Testing Statements for reverseArray3
+      // System.out.println("Reverse Part of the Array");
+      // reverseArray3(forward, 0, 3);
+      // System.out.println(Arrays.toString(forward));
+      // System.out.println("Reverse the Whole Array");
+      // reverseArray3(forward, 0, forward.length-1);
+      // System.out.println(Arrays.toString(forward));
    }
 
    static void reverseArray1(int[] X, int n, int[] Y) {
@@ -43,7 +51,18 @@ class pa1 {
    }
 
    static void reverseArray3(int[] X, int i, int j) {
-
+      if (i+1 == j) {
+         int temp = X[i];
+         X[i] = X[j];
+         X[j] = temp;
+         return;
+      }
+      else {
+         int temp = X[i];
+         X[i] = X[j];
+         X[j] = temp;
+         reverseArray3(X, i+1, j-1);
+      }
    }
 
    static void maxArrayIndex(int[] X, int p, int r) {
