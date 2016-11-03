024dc00beec5c886678b0c5098bc39f1daf1f02c
Spencer Albrecht <Spencer@college8-35-62.resnet.ucsc.edu>
2016-11-03 11:42:45 -0700

    Finished editing in accordance to style guidelines

--- pa3/DuplicateKeyException.java
+++ pa3/DuplicateKeyException.java
@@ -1,3 +1,10 @@
+// Spencer Albrecht
+// salbrech
+// PA3
+// DuplicateKeyException.java
+// Custom class for my custom exception that comes with a
+// default no parameter constructor as well as one that can
+// display a message
 class DuplicateKeyException extends RuntimeException {
   //Parameterless Constructor
   public DuplicateKeyException() {}
