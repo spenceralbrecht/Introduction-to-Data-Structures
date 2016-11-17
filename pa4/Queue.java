e1adcf84d96535dc729f49a14e5cfb1b63633b98
Spencer Albrecht <Spencer@college8-32-236.resnet.ucsc.edu>
2016-11-17 11:37:16 -0800

    PA4 - Created and completed custom exception class file

---
+++ pa4/Queue.java
@@ -0,0 +1,52 @@
+class Queue implements QueueInterface {
+  // isEmpty()
+  // pre: none
+  // post: returns true if this Queue is empty, false otherwise
+  public boolean isEmpty() {
+
+  }
+
+  // length()
+  // pre: none
+  // post: returns the length of this Queue.
+  public int length() {
+
+  }
+
+  // enqueue()
+  // adds newItem to back of this Queue
+  // pre: none
+  // post: !isEmpty()
+  public void enqueue(T newItem) {
+
+  }
+
+  // dequeue()
+  // deletes and returns item from front of this Queue
+  // pre: !isEmpty()
+  // post: this Queue will have one fewer element
+  public T dequeue() throws QueueEmptyException {
+
+  }
+
+  // peek()
+  // pre: !isEmpty()
+  // post: returns item at front of Queue
+  public T peek() throws QueueEmptyException {
+
+  }
+
+  // dequeueAll()
+  // sets this Queue to the empty state
+  // pre: !isEmpty()
+  // post: isEmpty()
+  public void dequeueAll() throws QueueEmptyException {
+
+  }
+
+  // toString()
+  // overrides Object's toString() method
+  public String toString() {
+    
+  }
+}
