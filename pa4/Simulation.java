f2eeeba6c1d7107e72e8c3462a441fb5495acd2a
Spencer Albrecht <Spencer@college8-32-236.resnet.ucsc.edu>
2016-11-17 19:54:25 -0800

    Added proper files to .gitignore and renamed Simulation file given from website

--- pa4/Simulation.java
+++ pa4/Simulation.java
@@ -41,22 +41,22 @@ public class Simulation{
 //
 //    4.  run simulation with n processors for n=1 to n=m-1  {
 //
-//    5.      declare and initialize an array of n processor Queues and any 
+//    5.      declare and initialize an array of n processor Queues and any
 //            necessary storage Queues
 //
 //    6.      while unprocessed jobs remain  {
 //
-//    7.          determine the time of the next arrival or finish event and 
+//    7.          determine the time of the next arrival or finish event and
 //                update time
 //
 //    8.          complete all processes finishing now
 //
-//    9.          if there are any jobs arriving now, assign them to a processor 
+//    9.          if there are any jobs arriving now, assign them to a processor
 //                Queue of minimum length and with lowest index in the queue array.
 //
 //    10.     } end loop
 //
-//    11.     compute the total wait, maximum wait, and average wait for 
+//    11.     compute the total wait, maximum wait, and average wait for
 //            all Jobs, then reset finish times
 //
 //    12. } end loop
@@ -65,4 +65,3 @@ public class Simulation{
 
    }
 }
-
